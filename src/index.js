3d96f0b0cd5fd175cbbcf3ea8b4d529a5a34f4e9
Oleksandr-Pik <pik.aleksandr79@gmail.com>
2024-03-16 17:28:08 +0200

    fix

--- src/index.js
+++ src/index.js
@@ -7,7 +7,7 @@ import "./index.scss";
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <BrowserRouter basename="/">
+    <BrowserRouter basename="/ignat-react-app">
       <App />
     </BrowserRouter>
   </React.StrictMode>
